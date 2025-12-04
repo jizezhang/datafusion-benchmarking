0621405d47e0d978634f6abd369c537423507e0a
Andrew Lamb <andrew@nerdnetworks.org>
2025-12-04 14:49:53 -0500

    add case when

--- scripts/scrape_comments.py
+++ scripts/scrape_comments.py
@@ -48,7 +48,7 @@ ALLOWED_BENCHMARKS_DF = {
     "clickbench_1",
     "clickbench_pushdown",
 }
-ALLOWED_CRITERION_BENCHMARKS_DF = {"sql_planner"}
+ALLOWED_CRITERION_BENCHMARKS_DF = {"sql_planner", "case_when"}
 ALLOWED_CRITERION_BENCHMARKS_ARROW = {"filter_kernels", "arrow_reader", "concatenate_kernels"}
 
 DEFAULT_REPOS = "apache/datafusion:apache/arrow-rs"
